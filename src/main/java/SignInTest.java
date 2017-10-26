f0afa2a29a944fd028aeea058346d61af24e81cc
vijay007hk <30658532+vijay007hk@users.noreply.github.com>
2017-10-26 14:53:58 +0530

    Update SignInTest.java

--- src/main/java/SignInTest.java
+++ src/main/java/SignInTest.java
@@ -1,28 +1,47 @@
 import com.sun.javafx.PlatformUtil;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class SignInTest extends DriverPath{
-
+	
     WebDriver driver ;
+    static String url;
+	static String signinerror;
+    static{
+    try {
+		FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"\\src\\main\\java\\TestVagrant\\Hotels\\Data.properties");
+		Properties data = new Properties();
+		data.load(fis);
+		url=data.getProperty("URL");
+		signinerror=data.getProperty("signinerror");
+	} catch (FileNotFoundException e) {
+		e.printStackTrace();
+	} catch (IOException e) {
+		e.printStackTrace();
+	}
+   }
+    
     @BeforeClass
-    public void initialize(){
-  	  DriverPath.setDriverPath();
-      driver = new ChromeDriver();
+    public void initialize() throws IOException{
+    	DriverPath.setDriverPath();
+        driver = new ChromeDriver();
     }
-
     @Test
     public void shouldThrowAnErrorIfSignInDetailsAreMissing() {
-        
-        driver.get("https://www.cleartrip.com/");
+    	
+        driver.get(url);
         waitFor(2000);
 
         driver.findElement(By.linkText("Your trips")).click();
         driver.findElement(By.id("SignIn")).click();
-
         waitFor(10000);
         try{
         	driver.switchTo().frame("modal_window");
@@ -31,9 +50,9 @@ public class SignInTest extends DriverPath{
         catch(Exception e){
          	System.out.println("Unable to click on Sign in button : "+e);
         }
-
+        
         String errors1 = driver.findElement(By.id("errors1")).getText();
-        Assert.assertTrue(errors1.contains("There were errors in your submission"));
+        Assert.assertTrue(errors1.contains(signinerror));
         driver.quit();
     }
    
