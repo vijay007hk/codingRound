2439528243033150be876ab5e8e192177a5df27c
vijay007hk <30658532+vijay007hk@users.noreply.github.com>
2017-10-26 15:07:54 +0530

    Reading data from Data.properties

--- src/main/java/FlightBookingTest.java
+++ src/main/java/FlightBookingTest.java
@@ -6,28 +6,50 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
-//Create a class DriverPathwith the reusble method setDriverPath
-public class FlightBookingTest extends DriverPath {
+import java.util.Properties;
 
-     WebDriver driver ;
+public class FlightBookingTest extends DriverPath{
+
+	 WebDriver driver ;
+	 static String url;
+	 static String from;
+	 static String to;
+	    static{
+	    try {
+			FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"\\src\\main\\java\\TestVagrant\\Hotels\\Data.properties");
+			Properties data = new Properties();
+			data.load(fis);
+			url=data.getProperty("URL");
+			from=data.getProperty("from");
+			to=data.getProperty("to");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	  }
 	    @BeforeClass
 	    public void initialize(){
-	  	  DriverPath.setDriverPath();
-	          driver = new ChromeDriver();
+	    	DriverPath.setDriverPath();
+	        driver = new ChromeDriver();
 	    }
 
     @Test
     public void testThatResultsAppearForAOneWayJourney() {
-      
-        driver.get("https://www.cleartrip.com/");
+       
+        driver.get(url);
         waitFor(2000);
         driver.findElement(By.id("OneWay")).click();
-
         driver.findElement(By.id("FromTag")).clear();
-        driver.findElement(By.id("FromTag")).sendKeys("Bangalore");
+        driver.findElement(By.id("FromTag")).sendKeys(from);
 
         //wait for the auto complete options to appear for the origin
 
@@ -36,7 +58,7 @@ public class FlightBookingTest extends DriverPath {
         originOptions.get(0).click();
 
         driver.findElement(By.id("ToTag")).clear();
-        driver.findElement(By.id("ToTag")).sendKeys("Delhi");
+        driver.findElement(By.id("ToTag")).sendKeys(to);
 
         //wait for the auto complete options to appear for the destination
 
@@ -44,10 +66,9 @@ public class FlightBookingTest extends DriverPath {
         //select the first item from the destination auto complete list
         List<WebElement> destinationOptions = driver.findElement(By.id("ui-id-2")).findElements(By.tagName("li"));
         destinationOptions.get(0).click();
-
-        //Changed div[1] to div[2], so that always an active date is slected
-	    driver.findElement(By.xpath("//*[@id='ui-datepicker-div']/div[2]/table/tbody/tr[3]/td[7]/a")).click();
-
+	//change div[1] to div[2] so that there is always an active date to be selected
+        driver.findElement(By.xpath("//*[@id='ui-datepicker-div']/div[2]/table/tbody/tr[3]/td[7]/a")).click();
+ 
         //all fields filled in. Now click on search
         driver.findElement(By.id("SearchBtn")).click();
 
@@ -59,7 +80,8 @@ public class FlightBookingTest extends DriverPath {
         driver.quit();
 
     }
-//Moved reusable waitFor() to DriverPath class
+
+    
     private boolean isElementPresent(By by) {
         try {
             driver.findElement(by);
@@ -69,4 +91,5 @@ public class FlightBookingTest extends DriverPath {
         }
     }
 
-  }
+    
+}
