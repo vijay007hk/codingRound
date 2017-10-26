ac08f8a6421c6b5fc149423f9616fd0bc045b0fc
vijay007hk <30658532+vijay007hk@users.noreply.github.com>
2017-10-26 15:01:25 +0530

    Reading Data from Data.properties

--- src/main/java/HotelBookingTest.java
+++ src/main/java/HotelBookingTest.java
@@ -1,14 +1,39 @@
 import com.sun.javafx.PlatformUtil;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class HotelBookingTest extends DriverPath{
+public class HotelBookingTest extends DriverPath  {
 
-    WebDriver driver;
+    protected WebDriver driver;
+    static String url;
+    static String localitytextbox;
+    static String travellersection;
+    static{
+    try {
+		FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"\\src\\main\\java\\TestVagrant\\Hotels\\Data.properties");
+		Properties data = new Properties();
+		data.load(fis);
+		url=data.getProperty("URL");
+		localitytextbox=data.getProperty("localitytextbox");
+		travellersection=data.getProperty("travellersection");
+	} catch (FileNotFoundException e) {
+		e.printStackTrace();
+	} catch (IOException e) {
+		e.printStackTrace();
+	}
+   }
+
     @FindBy(linkText = "Hotels")
     private WebElement hotelLink;
 
@@ -20,28 +45,30 @@ public class HotelBookingTest extends DriverPath{
 
     @FindBy(id = "travellersOnhome")
     private WebElement travellerSelection;
-    
-    @BeforeClass
-    public void initialize(){
+   
+        
+  @BeforeClass
+  public void initialize(){
 	  DriverPath.setDriverPath();
 	  driver = new ChromeDriver();
 	  PageFactory.initElements(driver,this);
-    }
-
+  }
+  
     @Test
     public void shouldBeAbleToSearchForHotels() {
-       
-        driver.get("https://www.cleartrip.com/");
-	    waitFor(5000);
+    	
+    	driver.get(url);
+        waitFor(5000);
         hotelLink.click();
+        waitFor(5000);
+        
+        localityTextBox.sendKeys(localitytextbox);
 
-        localityTextBox.sendKeys("Indiranagar, Bangalore");
-
-        new Select(travellerSelection).selectByVisibleText("1 room, 2 adults");
+        new Select(travellerSelection).selectByVisibleText(travellersection);
         searchButton.click();
 
         driver.quit();
 
     }
-
- }
+       
+}
